2ac6f8d90227723fe2de74ef4d94a064c283a75d
Nidiaj <juarez.nidia@gmail.com>
2015-06-11 12:00:21 -0700

    more updates

---
+++ app/controllers/contacts_controller.rb
@@ -0,0 +1,60 @@
+class ContactsController < ApplicationController
+  def index
+    @contacts = Contact.all
+    render('contacts/index.html.erb')
+  end
+
+  def show
+    @contact = Contact.find(params[:id])
+    render('contacts/show.html.erb')
+  end
+
+  # def new
+  #   render('contacts/new.html.erb')
+  # end
+
+  def create
+    @contact = Contact.create(:name => params[:name],
+        :sex => params[:sex],
+        :height => params[:height])
+    render('contacts/success.html.erb')
+  end
+
+  def new
+    @contact = Contact.new
+    render('contacts/new.html.erb')
+  end
+
+  def create
+    @contact = Contact.new(:name => params[:name],
+        :sex => params[:sex],
+        :height => params[:height])
+    if @contact.save
+      render('contacts/success.html.erb')
+    else
+      render('contacts/new.html.erb')
+    end
+  end
+
+  def edit
+    @contact = Contact.find(params[:id])
+    render('contacts/edit.html.erb')
+  end
+
+  def update
+    @contact = Contact.find(params[:id])
+    if @contact.update(:name => params[:name],
+          :sex => params[:sex],
+          :height => params[:height])
+      render('contacts/success.html.erb')
+    else
+      render('contacts/edit.html.erb')
+    end
+  end
+
+  def destroy
+    @contact = Contact.find(params[:id])
+    @contact.destroy
+    render('contacts/destroy.html.erb')
+  end
+end
